c6b19fbd34d5720fb523a82cb12a608ab520fc65
ArchLeaders <archleadership28@gmail.com>
2023-11-30 22:09:55 -0800

    Create debug runner and benchmarks

---
+++ src/MessageStudio.Runner/Benchmarks/MsbtParserBenchmarks.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+using MessageStudio.Core.Common;
+using MessageStudio.Core.Formats.Msbt;
+using MessageStudio.Core.Formats.Msbt.Structures.Sections;
+
+namespace MessageStudio.Runner.Benchmarks;
+
+[MemoryDiagnoser(true)]
+public class MsbtParserBenchmarks
+{
+    private byte[] _buffer = [];
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _buffer = File.ReadAllBytes("D:\\bin\\Msbt\\100enemy-LE.msbt");
+    }
+
+    [Benchmark]
+    public void Parse()
+    {
+        Parser parser = new(_buffer);
+        MsbtReader reader = new(ref parser);
+        foreach (MsbtLabelSection.MsbtLabel label in reader.LabelSection) {
+            int index = label.Index;
+            string value = label.Value;
+        }
+    }
+}
---
+++ src/MessageStudio.Runner/Program.cs
@@ -0,0 +1,22 @@
+﻿using MessageStudio.Core.Common;
+using MessageStudio.Core.Formats.Msbt;
+using MessageStudio.Core.Formats.Msbt.Structures.Sections;
+using System.Text;
+
+// BenchmarkDotNet.Running.BenchmarkRunner.Run<MessageStudio.Runner.Benchmarks.MsbtParserBenchmarks>();
+// return;
+
+byte[] buffer = File.ReadAllBytes(args[0]);
+Parser parser = new(buffer);
+MsbtReader reader = new(ref parser);
+
+Console.WriteLine($"Magic: {Encoding.UTF8.GetString(reader.Header.Magic)}");
+Console.WriteLine($"Byte Order Mark: {reader.Header.ByteOrderMark}");
+Console.WriteLine($"Encoding: {reader.Header.Encoding}");
+Console.WriteLine($"Version: {reader.Header.Version}");
+Console.WriteLine($"Section Count: {reader.Header.SectionCount}");
+Console.WriteLine($"File Size: {reader.Header.FileSize}");
+
+foreach (MsbtLabelSection.MsbtLabel label in reader.LabelSection) {
+    Console.WriteLine($"{label.Index}: {label.Value}");
+}
