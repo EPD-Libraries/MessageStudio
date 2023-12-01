9dd46e71164ef05a44c028948449b43da16dc998
ArchLeaders <archleadership28@gmail.com>
2023-11-30 22:07:47 -0800

    Create common SectionHeader struct

---
+++ src/MessageStudio.Core/Formats/Msbt/Structures/Common/SectionHeader.cs
@@ -0,0 +1,10 @@
+﻿using MessageStudio.Core.Common;
+using System.Runtime.InteropServices;
+
+namespace MessageStudio.Core.Formats.Msbt.Structures.Common;
+
+[StructLayout(LayoutKind.Sequential, Size = 12)]
+public readonly struct SectionHeader
+{
+    public readonly int Size;
+}
