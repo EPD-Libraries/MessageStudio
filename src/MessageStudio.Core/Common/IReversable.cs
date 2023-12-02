534827114418a9229d1f493f36d16cddaa4642c5
ArchLeaders <archleadership28@gmail.com>
2023-12-01 17:56:33 -0800

    Support byte-order reversing

---
+++ src/MessageStudio.Core/Common/IReversable.cs
@@ -0,0 +1,6 @@
+﻿namespace MessageStudio.Core.Common;
+
+public interface IReversable
+{
+    public static abstract void Reverse(in Span<byte> buffer);
+}
--- src/MessageStudio.Core/Common/Parser.cs
+++ src/MessageStudio.Core/Common/Parser.cs
@@ -5,16 +5,16 @@ namespace MessageStudio.Core.Common;
 
 public enum Endian : ushort
 {
-    Big = 0xFFFE,
-    Little = 0xFEFF,
+    Big = 0xFEFF,
+    Little = 0xFFFE,
 }
 
-public ref struct Parser(ReadOnlySpan<byte> buffer)
+public ref struct Parser(Span<byte> buffer, Endian endian = Endian.Big)
 {
-    private readonly ReadOnlySpan<byte> _buffer = buffer;
+    private readonly Span<byte> _buffer = buffer;
     private int _position = 0;
 
-    public Endian Endian = Endian.Big;
+    public Endian Endian = endian;
 
     public readonly int Position {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -34,16 +34,39 @@ public ref struct Parser(ReadOnlySpan<byte> buffer)
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public unsafe T Read<T>(in int offset = -1) where T : struct
+    public T Read<T>(int offset = -1) where T : unmanaged
     {
         int rOffset = ResolveOffset(offset);
-        T result = MemoryMarshal.Read<T>(_buffer[rOffset..]);
-        _position = rOffset + sizeof(T);
-        return result;
+        Span<byte> buffer = _buffer[rOffset..(_position = rOffset + Unsafe.SizeOf<T>())];
+
+        if (buffer.Length > 1 && IsInverseByteOrder()) {
+            buffer.Reverse();
+            T result = MemoryMarshal.Read<T>(buffer);
+            buffer.Reverse();
+            return result;
+        }
+
+        return MemoryMarshal.Read<T>(buffer);
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public ReadOnlySpan<byte> ReadSpan(int length, int offset = -1)
+    public unsafe T ReadStruct<T>(int offset = -1) where T : struct, IReversable
+    {
+        int rOffset = ResolveOffset(offset);
+        Span<byte> buffer = _buffer[rOffset..(_position = rOffset + sizeof(T))];
+
+        if (buffer.Length > 1 && IsInverseByteOrder()) {
+            T.Reverse(buffer);
+            T result = MemoryMarshal.Read<T>(buffer);
+            T.Reverse(buffer);
+            return result;
+        }
+
+        return MemoryMarshal.Read<T>(buffer);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Span<byte> ReadSpan(int length, int offset = -1)
     {
         int rOffset = ResolveOffset(offset);
 
@@ -55,18 +78,44 @@ public ref struct Parser(ReadOnlySpan<byte> buffer)
         throw EoF();
     }
 
+    /// <summary>
+    /// <para>Reads a span of <typeparamref name="T"/> from the buffer, reversing the bytes into a copied buffer if necessary.</para>
+    /// <b>Warning: this function allocates memory when reading the oposite byte-order, avoid using if another solution is faster.</b>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="length"></param>
+    /// <param name="offset"></param>
+    /// <returns></returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public unsafe ReadOnlySpan<T> ReadSpan<T>(int length, int offset = -1) where T : struct
+    public Span<T> ReadSpan<T>(int length, int offset = -1) where T : struct, IReversable
     {
         int rOffset = ResolveOffset(offset);
-        int bufferSize = sizeof(T) * length;
+        int blockSize = Unsafe.SizeOf<T>();
+        int bufferSize = blockSize * length;
 
         if (rOffset + bufferSize <= _buffer.Length) {
             _position = rOffset + bufferSize;
+
+            if (blockSize > 1 && IsInverseByteOrder()) {
+                Span<byte> buffer = new byte[bufferSize];
+                _buffer[rOffset..(rOffset + bufferSize)].CopyTo(buffer);
+                ReverseStructSpanBuffer<T>(buffer, blockSize, length);
+                return MemoryMarshal.Cast<byte, T>(buffer);
+            }
+
             return MemoryMarshal.Cast<byte, T>(_buffer[rOffset..(rOffset + bufferSize)]);
         }
 
         throw EoF();
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void ReverseStructSpanBuffer<T>(in Span<byte> buffer, int blockSize, int blockChainLength) where T : struct, IReversable
+    {
+        for (int i = 0; i < blockChainLength; i++) {
+            int blockOffset = blockSize * i;
+            T.Reverse(buffer[blockOffset..(blockOffset + blockSize)]);
+        }
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -80,4 +129,12 @@ public ref struct Parser(ReadOnlySpan<byte> buffer)
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private readonly int ResolveOffset(in int optionalOffset)
         => optionalOffset < 0 ? _position : optionalOffset;
+
+    /// <summary>
+    /// Returns true or false based on the OS byte-order and read endianness
+    /// </summary>
+    /// <returns></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private readonly bool IsInverseByteOrder()
+        => Endian == Endian.Big && BitConverter.IsLittleEndian || Endian == Endian.Little && !BitConverter.IsLittleEndian;
 }
