7603b306e019c8a6d85810ca337b9eb4c1e2e24c
ArchLeaders <archleadership28@gmail.com>
2023-12-01 17:58:04 -0800

    Add BE and LE benchmarks

--- src/MessageStudio.Runner/Benchmarks/MsbtParserBenchmarks.cs
+++ src/MessageStudio.Runner/Benchmarks/MsbtParserBenchmarks.cs
@@ -8,18 +8,31 @@ namespace MessageStudio.Runner.Benchmarks;
 [MemoryDiagnoser(true)]
 public class MsbtParserBenchmarks
 {
-    private byte[] _buffer = [];
+    private byte[] _bufferLe = [];
+    private byte[] _bufferBe = [];
 
     [GlobalSetup]
     public void Setup()
     {
-        _buffer = File.ReadAllBytes("D:\\bin\\Msbt\\100enemy-LE.msbt");
+        _bufferLe = File.ReadAllBytes("D:\\bin\\Msbt\\100enemy-LE.msbt");
+        _bufferBe = File.ReadAllBytes("D:\\bin\\Msbt\\100enemy.msbt");
     }
 
     [Benchmark]
-    public void Parse()
+    public void ParseLE()
     {
-        Parser parser = new(_buffer);
+        Parser parser = new(_bufferLe);
+        MsbtReader reader = new(ref parser);
+        foreach (MsbtLabelSection.MsbtLabel label in reader.LabelSection) {
+            int index = label.Index;
+            string value = label.Value;
+        }
+    }
+
+    [Benchmark]
+    public void ParseBE()
+    {
+        Parser parser = new(_bufferBe);
         MsbtReader reader = new(ref parser);
         foreach (MsbtLabelSection.MsbtLabel label in reader.LabelSection) {
             int index = label.Index;
