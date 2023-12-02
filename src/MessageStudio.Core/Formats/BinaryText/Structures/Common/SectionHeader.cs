8ec7b4dfe634b002c9fd0158b252f553be27441f
ArchLeaders <archleadership28@gmail.com>
2023-12-02 02:00:56 -0800

    Define const SectionHeader.LayoutSize

--- src/MessageStudio.Core/Formats/BinaryText/Structures/Common/SectionHeader.cs
+++ src/MessageStudio.Core/Formats/BinaryText/Structures/Common/SectionHeader.cs
@@ -6,6 +6,8 @@ namespace MessageStudio.Core.Formats.BinaryText.Structures.Common;
 [StructLayout(LayoutKind.Sequential, Size = 12)]
 public readonly partial struct SectionHeader : IReversable
 {
+    public const int LayoutSize = 12;
+
     public readonly int Size;
 
     public static void Reverse(in Span<byte> buffer)
