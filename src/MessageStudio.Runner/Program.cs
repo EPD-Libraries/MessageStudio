b2f3153e695ef38586742093999442e9b45ee17c
ArchLeaders <archleadership28@gmail.com>
2023-12-04 03:08:45 -0800

    Update runner

--- src/MessageStudio.Runner/Program.cs
+++ src/MessageStudio.Runner/Program.cs
@@ -2,37 +2,35 @@
 BenchmarkDotNet.Running.BenchmarkRunner.Run<MessageStudio.Runner.Benchmarks.MsbtParserBenchmarks>();
 #else
 
-using MessageStudio.Core.Common;
 using MessageStudio.Core.Formats.BinaryText;
 using MessageStudio.Core.Formats.BinaryText.Structures;
 using MessageStudio.Core.Formats.BinaryText.Structures.Sections;
 
 byte[] buffer = File.ReadAllBytes(args[0]);
-MemoryReader parser = new(buffer);
-MsbtReader msbt = new(parser);
+Msbt msbt = Msbt.FromBinary(buffer);
 
 Console.WriteLine($"Magic: {MsbtHeader.Magic}");
-Console.WriteLine($"Byte Order Mark: {msbt.Header.ByteOrderMark}");
-Console.WriteLine($"Encoding: {msbt.Header.Encoding}");
-Console.WriteLine($"Version: {msbt.Header.Version}");
-Console.WriteLine($"Section Count: {msbt.Header.SectionCount}");
-Console.WriteLine($"File Size: {msbt.Header.FileSize}");
+Console.WriteLine($"Byte Order Mark: {msbt.ReadOnly.Header.ByteOrderMark}");
+Console.WriteLine($"Encoding: {msbt.ReadOnly.Header.Encoding}");
+Console.WriteLine($"Version: {msbt.ReadOnly.Header.Version}");
+Console.WriteLine($"Section Count: {msbt.ReadOnly.Header.SectionCount}");
+Console.WriteLine($"File Size: {msbt.ReadOnly.Header.FileSize}");
 
 Console.WriteLine("\nLabels:");
-foreach (MsbtLabel label in msbt.LabelSection) {
+foreach (MsbtLabel label in msbt.ReadOnly.LabelSection) {
     Console.WriteLine($"{label.Index}: {label.Value}");
 }
 
 Console.WriteLine("\nAttributes:");
-foreach (MsbtAttribute atr in msbt.AttributeSection!) {
+foreach (MsbtAttribute atr in msbt.ReadOnly.AttributeSection!) {
     Console.WriteLine($"{atr.Index}: {atr.Value}");
 }
 
 Console.WriteLine("\nText:");
-foreach (MsbtText txt in msbt.TextSection) {
+foreach (MsbtText txt in msbt.ReadOnly.TextSection) {
     Console.WriteLine($"{txt.Index}: {txt.Value}");
 }
 
-File.WriteAllText("D:\\bin\\Msbt\\test.yml", msbt.ToYaml());
+File.WriteAllText("D:\\bin\\Msbt\\test.yml", msbt.ReadOnly.ToYaml());
 
 #endif
