b3c159e172c6958dd2bc278ab72ccc078e37c142
ArchLeaders <archleadership28@gmail.com>
2023-12-04 02:49:24 -0800

    Impl Msbt

--- src/MessageStudio.Core/Formats/BinaryText/Msbt.cs
+++ src/MessageStudio.Core/Formats/BinaryText/Msbt.cs
@@ -2,21 +2,27 @@
 
 namespace MessageStudio.Core.Formats.BinaryText;
 
-public struct Msbt
+public class Msbt : Dictionary<string, MsbtEntry>
 {
+    public ReadOnlyMsbt ReadOnly { get; }
+
     public static Msbt FromBinary(in Memory<byte> buffer)
     {
         MemoryReader reader = new(buffer);
         return new(new(reader));
     }
 
-    public Msbt()
-    {
-
-    }
+    public Msbt() { }
 
     public Msbt(in ReadOnlyMsbt reader)
     {
+        ReadOnly = reader;
 
+        foreach (var label in reader.LabelSection) {
+            Add(label.Value, new MsbtEntry {
+                Attribute = reader.AttributeSection?[label.Index].Value,
+                Text = reader.TextSection[label.Index].Value
+            });
+        }
     }
 }
---
+++ src/MessageStudio.Core/Formats/BinaryText/MsbtEntry.cs
@@ -0,0 +1,14 @@
+﻿namespace MessageStudio.Core.Formats.BinaryText;
+
+public class MsbtEntry
+{
+    /// <summary>
+    /// The entry attribute from the attribute section (Optional)
+    /// </summary>
+    public string? Attribute { get; set; }
+
+    /// <summary>
+    /// The pseudo-HTML processed text
+    /// </summary>
+    public string? Text { get; set; }
+}
