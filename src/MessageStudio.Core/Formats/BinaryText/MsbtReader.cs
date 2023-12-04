ee130895ad13288bab8769f7e3f53ededd918910
ArchLeaders <archleadership28@gmail.com>
2023-12-04 02:19:45 -0800

    Add ToYaml support

--- src/MessageStudio.Core/Formats/BinaryText/MsbtReader.cs
+++ src/MessageStudio.Core/Formats/BinaryText/MsbtReader.cs
@@ -2,6 +2,7 @@
 using MessageStudio.Core.Formats.BinaryText.Structures;
 using MessageStudio.Core.Formats.BinaryText.Structures.Sections;
 using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace MessageStudio.Core.Formats.BinaryText;
 
@@ -47,4 +48,40 @@ public struct MsbtReader
 
     public static explicit operator Msbt(MsbtReader parser)
         => parser.GetWriter();
+
+    public readonly string ToYaml()
+    {
+        StringBuilder sb = new();
+        if (AttributeSection?.AttributeSize == 0) {
+            Write(ref sb);
+        }
+        else {
+            WriteWithAttributes(ref sb);
+        }
+
+        return sb.ToString();
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private readonly void WriteWithAttributes(ref StringBuilder sb)
+    {
+        foreach (var label in LabelSection) {
+            sb.Append(label.Value);
+            sb.AppendLine(":");
+            sb.Append("  Attribute: ");
+            sb.AppendLine(AttributeSection![label.Index].Value ?? "~");
+            sb.Append("  Text: |-\n    ");
+            sb.AppendLine(TextSection[label.Index].Value.Replace("\n", "\n    "));
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private readonly void Write(ref StringBuilder sb)
+    {
+        foreach (var label in LabelSection) {
+            sb.Append(label.Value);
+            sb.Append(": |-\n  ");
+            sb.AppendLine(TextSection[label.Index].Value.Replace("\n", "\n  "));
+        }
+    }
 }
